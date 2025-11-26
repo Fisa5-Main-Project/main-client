0f372fe60f9c99cf157863ad4d70ae22ba57806f
uxxin <dbwlsdid1114@naver.com>
2025-11-26 10:23:38 +0900

    refactor: 연금 UI 수정

--- src/app/pension/taxsaving/layout.tsx
+++ src/app/pension/taxsaving/layout.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-
-/**
- * Pension 라우트 그룹의 레이아웃
- */
+import Header from "@/components/common/Header";
 
 export default function PensionTaxsavingLayout({
   children,
@@ -10,7 +7,8 @@ export default function PensionTaxsavingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="page-container flex flex-col min-h-screen">
+    <main className="page-container bg-[linear-gradient(to_bottom,#FFFFFF_0%,#CCE1FF_17%,#E0EDFF_50%,#FFFFFF_79%,#FFFFFF_100%)] flex flex-col min-h-screen">
+      <Header />
       {children}
     </main>
   );
