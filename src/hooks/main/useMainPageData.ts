dad7d5c9f743276eecea1b0c228184aa3a1b1da0
OJiyeong <owldud215@gmail.com>
2025-12-02 18:18:41 +0900

    Refactor: mock 주석 제거

--- src/hooks/main/useMainPageData.ts
+++ src/hooks/main/useMainPageData.ts
@@ -12,7 +12,6 @@ export interface AggregatedAssetDetail {
   name: string; // 사용자 표시 이름 (예: '예적금')
   balance: number; // 해당 항목의 합산 잔액
   percentage: number; // 총자산 대비 비율
-  //icon: string; // 버블 UI용 아이콘 경로
 }
 
 interface MainData {
@@ -23,52 +22,6 @@ interface MainData {
   assetDetails?: AggregatedAssetDetail[];
 }
 
-// 임시 Mock Data
-const MOCK_DATA_CONNECTED = {
-  name: "홍길동",
-  asset_total: 42000000,
-  user_mydata_registration: true, // 연동 완료 가정
-  investment_tendency: "적극투자형",
-  assetDetails: [
-    {
-      type: "REAL_ESTATE",
-      balance: 33600000,
-      percentage: 60,
-      icon: "/main/Estate.png",
-      name: "부동산",
-    },
-    {
-      type: "SAVING",
-      balance: 6720000,
-      percentage: 30,
-      icon: "/main/Saving.png",
-      name: "적금",
-    },
-    {
-      type: "SAVING",
-      balance: 6720000,
-      percentage: 18,
-      icon: "/main/Saving.png",
-      name: "적금",
-    },
-    {
-      type: "INVESTMENT",
-      balance: 1680000,
-      percentage: 8,
-      icon: "/main/Invest.png",
-      name: "투자",
-    },
-    // ... 필요한 만큼 추가
-  ],
-};
-
-const MOCK_DATA_NOT_CONNECTED = {
-  name: "홍길동",
-  asset_total: null, // 자산 없음 가정
-  user_mydata_registration: false, // 연동 안 됨 가정
-  investment_tendency: null,
-  assetDetails: undefined,
-};
 
 // 메인 페이지에 필요한 사용자 데이터 및 마이데이터 연동 상태를 불러오는 훅
 export const useMainPageData = () => {
@@ -158,33 +111,9 @@ export const useMainPageData = () => {
           setData(null);
         }
 
-        // -----------------------------------------------------------------------------------------------------------
-
-        // //1. 500ms 지연 시간 시뮬레이션 (로딩 효과를 위해)
-        // await new Promise((resolve) => setTimeout(resolve, 500));
-
-        // // 2. 연동 상태에 따른 Mock Data 선택 (테스트 편의를 위해 임의로 선택)
-        // // const isConnected = false;
-        // const isConnected = true;
-
-        // const mockResponseData = isConnected
-        //   ? MOCK_DATA_CONNECTED
-        //   : MOCK_DATA_NOT_CONNECTED;
-        // // const mockResponseData = MOCK_DATA_CONNECTED;
-
-        // // 3. UI 렌더링을 위해 API 응답 데이터를 로컬 상태에 직접 저장
-        // setData({
-        //   name: mockResponseData.name,
-        //   assetTotal: mockResponseData.asset_total,
-        //   isMyDataRegistered: mockResponseData.user_mydata_registration,
-        //   investmentTendency: mockResponseData.investment_tendency,
-        //   assetDetails:
-        //     mockResponseData.assetDetails as AggregatedAssetDetail[],
-        // });
       } catch (error) {
         console.error("메인 페이지 데이터 로드 실패:", error);
         setData(null); // API 실패 시 데이터 null 처리
-        // console.error("Mock 데이터 로드 실패 (논리 오류):", error);
       } finally {
         setIsLoading(false);
       }
