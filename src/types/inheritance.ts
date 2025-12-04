a02dcc3987a2e543f0e854e18b34e054c659d4ed
uxxin <dbwlsdid1114@naver.com>
2025-12-04 12:33:50 +0900

    style: 이미지 없는 부분 추가

--- src/types/inheritance.ts
+++ src/types/inheritance.ts
@@ -10,14 +10,14 @@ export type SelectedHeir = Heir & { uniqueId: string };
 
 // 상속인 옵션 데이터
 export const heirOptions: Heir[] = [
-  { id: "spouse", label: "배우자", imgBase: "husband" },
+  { id: "spouse", label: "배우자", imgBase: "spouse" },
   { id: "child", label: "자녀", imgBase: "child" },
   { id: "grandchild", label: "손자녀", imgBase: "grandchild" },
   { id: "father", label: "아버지", imgBase: "father" },
   { id: "mother", label: "어머니", imgBase: "mother" },
   { id: "grandfather", label: "할아버지", imgBase: "grandfather" },
   { id: "grandmother", label: "할머니", imgBase: "grandmother" },
-  { id: "sibling", label: "형제 자매", imgBase: "siblings_girl" },
+  { id: "sibling", label: "형제 자매", imgBase: "sibling" },
   { id: "relative", label: "4촌 이내 혈족", imgBase: "relative" },
 ];
 
